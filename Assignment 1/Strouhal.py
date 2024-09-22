0ff2c82f04edc6d87c55179ebb75083aaf522041
VictorLier <victor@lier.dk>
2024-09-22 23:09:33 +0200

    Strouhal added

---
+++ Assignment 1/Strouhal.py
@@ -0,0 +1,17 @@
+import sympy as sp
+
+# Define the variables
+St, f, L, U = sp.symbols('St f L U')
+
+#Strouhal number
+eq1 = sp.Eq(St, f*L/U)
+
+# Solve the equation
+f = sp.solve(eq1, f)
+
+# Values
+L_val = 1
+U_val = 1
+St_val = 0.21 # Trukket fra chatten
+
+print(f[0].subs({L: L_val, U: U_val, St: St_val}))
